493e2024664d5d34eb9c6c7cc3b236b4b9235a32
Naji Kanounji <n-kanounji@hotmail.com>
2025-09-12 23:35:50 +0200

    fix typo

--- src/constants/ThemeContext.tsx
+++ src/constants/ThemeContext.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-f
+
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   document.documentElement.setAttribute('data-theme', 'winter');
 
